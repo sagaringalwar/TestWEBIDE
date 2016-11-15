3dbfce3243ec0b9fe3564b9a8121d2914fda70f9
Sagar Ingalwar <sagar.ingalwar@gmail.com>
2016-11-15 11:11:30 +0000

    test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TestWEBIDE
 Testing WEbIDE and GITHUB
+
+Adding second line for GIT Hub Web IDE integration
---
+++ view/testView1.view.js
@@ -0,0 +1,28 @@
+sap.ui.jsview("com.sap.lilly.view.testView1", {
+
+	/** Specifies the Controller belonging to this View. 
+	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	 * @memberOf com.sap.lilly.view.testView1
+	 */
+	getControllerName: function() {
+		return "com.sap.lilly.controller.testView1";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	 * Since the Controller is given to this method, its event handlers can be attached right away. 
+	 * @memberOf com.sap.lilly.view.testView1
+	 */
+	createContent: function(oController) {
+		var oPage = new sap.m.Page({
+			title: "Title",
+			content: []
+		});
+
+		var app = new sap.m.App("myApp", {
+			initialPage: "oPage"
+		});
+		app.addPage(oPage);
+		return app;
+	}
+
+});
